2bf94a7d65b5d339fbc98e53efac0a7f1d344b08
sarita <saritapatidar@thoughtwin.com>
2025-11-13 15:43:09 +0530

    another function

--- main.py
+++ main.py
@@ -22,5 +22,11 @@ l=max(a,b)
 print("The maximum of a and b is:", l)
 m=min(a,b)
 print("The minimum of a and b is:", m)
-n=abs(a-b)
+n=abs(a+b)
 print("The absolute difference between a and b is:", n)
+
+
+def multiply(x, y):
+    return x * y
+
+result = multiply(20,30)
