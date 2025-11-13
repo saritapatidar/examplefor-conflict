60123f1a554316d0678a8a416d2a578bd9a086bf
sarita <saritapatidar@thoughtwin.com>
2025-11-13 15:01:10 +0530

    Add basic arithmetic operations and their outputs

---
+++ main.py
@@ -0,0 +1,24 @@
+a=60
+b=90
+c=a+b
+print("The sum of a and b is:", c)
+d=b-a
+print("The difference between b and a is:", d)
+e=a*b
+print("The product of a and b is:", e)
+f=b/a
+print("The division of b by a is:", f)
+g=b%a
+print("The modulus of b by a is:", g)
+h=b//a
+print("The floor division of b by a is:", h)
+i=a**2
+print("The square of a is:", i)
+j=b**2
+print("The square of b is:", j)
+k=(a+b)/2
+print("The average of a and b is:", k)
+l=max(a,b)
+print("The maximum of a and b is:", l)
+m=min(a,b)
+print("The minimum of a and b is:", m)
